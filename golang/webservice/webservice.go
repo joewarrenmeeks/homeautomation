c920880f3cb50e008d2ec99fc1928d4e38a46097
joewarrenmeeks <joe.warren.meeks@gmail.com>
2015-11-21 20:43:55 +0000

    testing

--- golang/webservice/webservice.go
+++ golang/webservice/webservice.go
@@ -9,9 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		
 		fmt.Fprintf(w, "Bye, %q", html.EscapeString(r.URL.Path))
-		
 	})
 	
 	log.Fatal(http.ListenAndServe(":8080", nil))
