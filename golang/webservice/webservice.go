6d2879383325a29316b313659832c6afec74f607
joewarrenmeeks <joe.warren.meeks@gmail.com>
2015-11-21 21:02:56 +0000

    Basic HTTP router

--- golang/webservice/webservice.go
+++ golang/webservice/webservice.go
@@ -5,12 +5,25 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Bye, %q", html.EscapeString(r.URL.Path))
-	})
-	
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/william", William)
+	router.HandleFunc("/dad", Dad)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Please select a target")
+}
+
+func William(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You chose William. Bad choice. Bad.")
+}
+
+func Dad(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You chose Dad. Excellent choice.")
 }
